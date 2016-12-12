3203200ae096a91c30b0179f83698d0598e1b381
maithanhdat1996 <mtdat1996>
2016-12-12 22:39:12 +0700

    Edit AutoMapper in example IProductRepository => TestGetAllProduct()

--- SocialFashion.Data/Repositories/ProductRepository.cs
+++ SocialFashion.Data/Repositories/ProductRepository.cs
@@ -23,9 +23,12 @@ namespace SocialFashion.Data.Repositories
         public IEnumerable<Product> TestGetAllProduct()
         {
             IEnumerable<GetAllProduct_Result> lstProductSp =  db.GetAllProduct().ToList();
-            Mapper.CreateMap<Product, GetAllProduct_Result>().ReverseMap();
-            IEnumerable<Product> listProduct = Mapper.Map<IEnumerable<Product>>(lstProductSp);
-            return listProduct;
+
+            Mapper.CreateMap<IEnumerable<GetAllProduct_Result>, IEnumerable<Product>>();
+
+            IEnumerable<Product> lstProduct =  Mapper.Map<IEnumerable<GetAllProduct_Result>, IEnumerable<Product>>(lstProductSp);
+
+            return lstProduct;
          }
     }
 }

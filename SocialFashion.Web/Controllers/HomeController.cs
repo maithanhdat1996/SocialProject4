5456f464d3db1afc524f4e6ca4c97820e39f111e
maithanhdat1996 <mtdat1996>
2016-12-13 12:45:54 +0700

    guides take currentuserId

--- SocialFashion.Web/Controllers/HomeController.cs
+++ SocialFashion.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using SocialFashion.Model.Models;
+using SocialFashion.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +11,28 @@ namespace SocialFashion.Web.Controllers
 {
     public class HomeController : Controller
     {
+        IProductService _productService;
+        
+        public HomeController(IProductService productServicer)
+        {
+            this._productService = productServicer;
+
+        }
+
+         
         public ActionResult Index()
         {
+            if(Request.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+                using (SocialFashionDbContext db = new SocialFashionDbContext())
+                {
+                    AspNetUser user = db.AspNetUsers.SingleOrDefault(m => m.Id == userId);
+                }
+                   
+               
+
+            }
             return View();
         }
 
